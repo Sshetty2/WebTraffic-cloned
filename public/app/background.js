a3bd5997056e545b408a06fb6064d79b8023c6a7
Sachit <sachit.shetty@gmail.com>
2019-01-26 19:15:11 -0500

    switch back to production keys

--- public/app/background.js
+++ public/app/background.js
@@ -1,8 +1,8 @@
 const meetup_client_key = 'rd4j2luc2buqrg44s86ka6fhse'
 
-// const redirect_Uri =  'https://cabfodbfjmgloaallchcnnkgcfpnobem.chromiumapp.org/'
+const redirect_Uri =  'https://cabfodbfjmgloaallchcnnkgcfpnobem.chromiumapp.org/'
 
-const redirect_Uri =  'https://mkoendagbaclehcbfngejdkecaplledj.chromiumapp.org/'
+// const redirect_Uri =  'https://mkoendagbaclehcbfngejdkecaplledj.chromiumapp.org/'
 // development redirect_Uri
 // production client id for chrome app (update manifest) = 466748401928-m88okvel4gdsc9rjo9qlo8em25ihs65s.apps.googleusercontent.com
 // development client id for chrome app (update manifest) = 466748401928-clsj0b12h299emdtcngqcdkon8i8n0nk.apps.googleusercontent.com

b85899dcf63700da8b56d14ed1fd95ad719a28b2
Sachit <sachit.shetty@gmail.com>
2019-03-12 16:58:06 -0400

    switch to production keys and update version numbers

--- public/app/bgscript-constants.js
+++ public/app/bgscript-constants.js
@@ -1,19 +1,19 @@
 //dev
-const mCK = 'fa87euorcttsc8kpaks36d06nc'
+// const mCK = 'fa87euorcttsc8kpaks36d06nc'
 
 //prod
-// const mCK = 'rd4j2luc2buqrg44s86ka6fhse'
+const mCK = 'rd4j2luc2buqrg44s86ka6fhse'
 
-// const redirect_Uri =  'https://cabfodbfjmgloaallchcnnkgcfpnobem.chromiumapp.org/'
-const redirect_Uri = 'https://mkoendagbaclehcbfngejdkecaplledj.chromiumapp.org/'
+const redirect_Uri =  'https://cabfodbfjmgloaallchcnnkgcfpnobem.chromiumapp.org/'
+// const redirect_Uri = 'https://mkoendagbaclehcbfngejdkecaplledj.chromiumapp.org/'
 
-// production = 466748401928-m88okvel4gdsc9rjo9qlo8em25ihs65s.apps.googleusercontent.com
+production = 466748401928-m88okvel4gdsc9rjo9qlo8em25ihs65s.apps.googleusercontent.com
 // development = 466748401928-clsj0b12h299emdtcngqcdkon8i8n0nk.apps.googleusercontent.com
 
 //dev
-const mCS = 'gimsq0ijqf99brtsbvofrveh5p'
+// const mCS = 'gimsq0ijqf99brtsbvofrveh5p'
 //prod
-// const mCS = 'tm034sb7uq41r55qeea3etjd28'
+const mCS = 'tm034sb7uq41r55qeea3etjd28'
 
 const gATEP = 'https://secure.meetup.com/oauth2/access'
 const gAK = 'AIzaSyBDxenr7SA1hbdkm_k-1eP7DZTfKaju-UE'

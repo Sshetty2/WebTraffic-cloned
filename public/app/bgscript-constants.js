cb0ae2993df9079e18e074d55901fac61bafea35
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-03-26 22:54:01 -0400

    notes

--- public/app/bgscript-constants.js
+++ public/app/bgscript-constants.js
@@ -1,16 +1,16 @@
 //dev
 // const mCK = 'fa87euorcttsc8kpaks36d06nc'
 //dev desktop
-const mCK = 'gh7mcktjlopdddno5sv8ufcu80'
+// const mCK = 'gh7mcktjlopdddno5sv8ufcu80'
 //prod
-// const mCK = 'rd4j2luc2buqrg44s86ka6fhse'
+const mCK = 'rd4j2luc2buqrg44s86ka6fhse'
 
 //prod 
-// const redirect_Uri =  'https://cabfodbfjmgloaallchcnnkgcfpnobem.chromiumapp.org/'
+const redirect_Uri =  'https://cabfodbfjmgloaallchcnnkgcfpnobem.chromiumapp.org/'
 // dev 
 // const redirect_Uri = 'https://mkoendagbaclehcbfngejdkecaplledj.chromiumapp.org/'
 // dev desktop 
-const redirect_Uri = 'https://aeklccpcadobjohfhnfppdhphkeabomk.chromiumapp.org/'
+// const redirect_Uri = 'https://aeklccpcadobjohfhnfppdhphkeabomk.chromiumapp.org/'
 
 
 // production = 466748401928-m88okvel4gdsc9rjo9qlo8em25ihs65s.apps.googleusercontent.com
@@ -19,9 +19,9 @@ const redirect_Uri = 'https://aeklccpcadobjohfhnfppdhphkeabomk.chromiumapp.org/'
 //dev
 // const mCS = 'gimsq0ijqf99brtsbvofrveh5p'
 //prod
-// const mCS = 'tm034sb7uq41r55qeea3etjd28'
+const mCS = 'tm034sb7uq41r55qeea3etjd28'
 //dev desktop
-const mCS = 'm6045u5anl7ptbgj04jfg70mo9'
+// const mCS = 'm6045u5anl7ptbgj04jfg70mo9'
 
 const gATEP = 'https://secure.meetup.com/oauth2/access'
 const gAK = 'AIzaSyBDxenr7SA1hbdkm_k-1eP7DZTfKaju-UE'

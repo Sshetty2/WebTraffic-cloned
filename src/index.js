d0d6b6c24bf71d473c3f3f861185f0cd8c8c1b54
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-04-05 10:15:02 -0400

    add conditional ro render app only if popup with title MBEST is found

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 
 
-
+if(document.getElementsByTagName('title')[0].innerHTML === 'Meetup Batch Event Set Tool')
 ReactDOM.render(<App />, document.getElementById('root'));

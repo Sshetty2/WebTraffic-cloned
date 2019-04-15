24bc0f2d6bfa067c87923579bfb6334282ce4368
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-04-15 01:17:09 -0400

    switch to desktop dev keys

--- public/app/bgscript-constants.js
+++ public/app/bgscript-constants.js
@@ -1,7 +1,7 @@
 //dev laptop
-const mCK = 'kgkm17qkpg4hh781h4tu67tki3'
+// const mCK = 'kgkm17qkpg4hh781h4tu67tki3'
 //dev desktop
-// const mCK = 'gh7mcktjlopdddno5sv8ufcu80'
+const mCK = 'gh7mcktjlopdddno5sv8ufcu80'
 //prod
 // const mCK = 'rd4j2luc2buqrg44s86ka6fhse'
 
@@ -9,20 +9,20 @@ const mCK = 'kgkm17qkpg4hh781h4tu67tki3'
 //prod 
 // const redirect_Uri =  'https://cabfodbfjmgloaallchcnnkgcfpnobem.chromiumapp.org/'
 // dev laptop 
-const redirect_Uri = 'https://jodnpnodmbflogmledmffojgmdjljfmj.chromiumapp.org/'
+// const redirect_Uri = 'https://jodnpnodmbflogmledmffojgmdjljfmj.chromiumapp.org/'
 // dev desktop 
-// const redirect_Uri = 'https://aeklccpcadobjohfhnfppdhphkeabomk.chromiumapp.org/'
+const redirect_Uri = 'https://aeklccpcadobjohfhnfppdhphkeabomk.chromiumapp.org/'
 
 // production = 466748401928-m88okvel4gdsc9rjo9qlo8em25ihs65s.apps.googleusercontent.com
 // development = 466748401928-7hgv6idtmp9dolbvedg85oahappoprel.apps.googleusercontent.com
 // dev desktop = 466748401928-03ccuqqlm38fvnkprf2ole9knehounkt.apps.googleusercontent.com
 
 //dev laptop
-const mCS = 'etchk9798mm1oqdfbg23bq11k2'
+// const mCS = 'etchk9798mm1oqdfbg23bq11k2'
 //prod
 // const mCS = 'tm034sb7uq41r55qeea3etjd28'
 //dev desktop
-// const mCS = 'm6045u5anl7ptbgj04jfg70mo9'
+const mCS = 'm6045u5anl7ptbgj04jfg70mo9'
 
 const gATEP = 'https://secure.meetup.com/oauth2/access'
 const gAK = 'AIzaSyBDxenr7SA1hbdkm_k-1eP7DZTfKaju-UE'

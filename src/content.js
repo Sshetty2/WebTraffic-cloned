633bbe2b18e5cb2cbdbabc9ae18305d7f089a98b
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-04-17 23:49:44 -0400

    delete log statement and add note

--- src/content.js
+++ src/content.js
@@ -6,12 +6,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
-
+// creates a div with an id of 'meetup-batch-event-set' then injects the app into that container
 let column = document.getElementById('simple-event-filter-column') || ''
 let block_to_insert = document.createElement( 'div' );
 block_to_insert.id = 'meetup-batch-event-set'
 if(column) { column.appendChild( block_to_insert );
-console.log('app was set');
 ReactDOM.render(<div>{column ? <App />: ""}</div>, block_to_insert) }
 

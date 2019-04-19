3cf55ace28edb9b4c3072d45c89840626e5158ba
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-04-19 10:53:43 -0400

    update style for button injection and delete extraneous mutation observer declaration

--- public/app/content.js
+++ public/app/content.js
@@ -64,7 +64,7 @@ function buttonInjection(){
             let groupName = x.children[1].children[0].children[1].href.match(/(?<=\meetup\.com\/)(.*?)(?=\s*\/)/)[0]
             let startTime = new Date(x.children[0].children[0].children[0].dateTime).getTime()
             buttonContainer.id = `${groupName} ${startTime}`
-            buttonContainer.setAttribute("style", 'background-color: #00455d;color: white;font-family: "Roboto", "Helvetica", "Arial", sans-serif;font-weight: 400;font-size: medium; padding: 10px;');
+            buttonContainer.setAttribute("style", 'background-color: #0e2a4c;color: white;font-family: "Roboto", "Helvetica", "Arial", sans-serif;font-weight: 400; font-size: 14px; padding: 5px;');
             buttonContainer.onclick = onClick
             block_to_insert.appendChild(buttonContainer)
             x.children[2].appendChild(block_to_insert)
@@ -76,35 +76,15 @@ function buttonInjection(){
       }catch(err){ console.log(err)}
     }
 
-
-
-// this mutation observer will listen for anytime the height of the main container is changed and then inject that same height into the right container where the application is injected
-
-var observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutationRecord) {
-    const docHeight = mutationRecord.target.scrollHeight
-    document.getElementById('simple-event-filter-column').setAttribute("style", `height: ${docHeight}px`);
-    buttonInjection();
-  });    
-});
-
-// instantiating the mutation observer
-var target = document.getElementsByClassName('searchResults')[0];
-if(target) observer.observe(target, { attributes : true });
-
-
+// on update listener fires every time the page gets updated in any way
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.type === 'onUpdateFrmEvent') {
     sendResponse('the the onupdate listener is working correctly and we recieved the message to fire our messages')
     //chrome message to set state of textField to empty so that a user can type in it
-    chrome.runtime.sendMessage({type: 'resetTextField'}, (response) => {
-      //console.log(response)
-    }) 
-    
+    chrome.runtime.sendMessage({type: 'resetTextField'})
     let pathname = window.location.pathname
     let groupName, grpNameArray, grpNameCollection
     // checks current pathname if it has the following strings and if it doesn't and its not empty, then groupName is assigned to the pathname 
-    
     if(!pathname.match( /(find|login|create|messages|account|members|topics|apps|meetup_api)/ ) && pathname.slice(1)) { 
       pathname = pathname.slice(1)
       groupName = pathname.slice(0, pathname.indexOf('/')) 
@@ -118,17 +98,16 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     chrome.storage.local.set({grpNameArray: grpNameArray})
     buttonInjection()
   }
-  
-  // style injections
+    // style injections
   if(document.getElementsByClassName('rock-salt')[0]) {
     document.getElementsByClassName('rock-salt')[0].setAttribute("style", "margin: 0px 0px 0px 0px; padding-bottom: 15px; line-height: 26pt; font-family: \"Graphik Meetup\",helvetica,arial,sans-serif ; font-size: 28px; color: #00455d");
     document.getElementsByClassName('react-calendar')[0].setAttribute("style", 'background-color: #f5f5dc; border-radius: 10px;');
     document.getElementById('mbest-form-button').setAttribute("style", "background-color: #0f1721;");
-  // get the height of the document by querying the node where all of the meetup events reside
+    // get the height of the document by querying the node where all of the meetup events reside
     const docHeight = document.getElementsByClassName('searchResults')[0].scrollHeight
     document.getElementById('meetup-batch-event-set').setAttribute("style", 'position: -webkit-sticky; position: sticky; top: 135px; padding-bottom: 0px#b5d2c8; padding-bottom: 5px; box-shadow: 0px 0px 20px 3px rgba(0,0,0,0.64)');    document.getElementsByClassName('App')[0].setAttribute("style", 'padding-top: 10px;padding-bottom: 0px;#e4e6e6; padding-bottom: 5px; box-shadow: 0px 0px 17px -7px rgba(0,0,0,0.23);');
     document.getElementById('simple-event-filter-column').setAttribute("style", `height: ${docHeight}px`)
-
+    // this mutation observer will listen for anytime the height of the main container is changed and then inject that same height into the right container where the application is injected
     var observer = new MutationObserver(function(mutations) {
       mutations.forEach(function(mutationRecord) {
         const docHeight = mutationRecord.target.scrollHeight
@@ -140,7 +119,6 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     var target = document.getElementsByClassName('searchResults')[0];
     observer.observe(target, { attributes : true });
   }
-
   return true;
 });
 

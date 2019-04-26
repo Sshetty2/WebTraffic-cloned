a0cc362ec365c19c10806f4cab3c64cd7786071d
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-04-25 22:54:40 -0400

    relays separated into a separate file
    these relays are neccessary because applications injected into the content of the current webpage context can not receive messages automatically from the messages that are sent from the application. Relays need to be configured that essentially send messages from the app to the bg to the content.

---
+++ background_scripts/bgscript-relays.js
@@ -0,0 +1,17 @@
+/*global chrome*/
+
+const loadScreenRelay = () => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { type: 'loadingScreenInit' }, (response) => {
+            console.log(response);
+        });
+    });
+}
+
+const resetTextFieldRelay = () => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { type: 'resetTextField' }, (response) => {
+          console.log(response);
+        });
+    });
+}

b9183da28a1fb4b3829ac726785883cc32725e4c
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-05-03 22:42:12 -0400

    fix formatting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
 import App from "./App";
 
-if (
-	document.getElementsByTagName("title")[0].innerHTML ===
-	"Meetup Batch Event Set Tool"
-)
+if (document.getElementsByTagName("title")[0].innerHTML === "Meetup Batch Event Set Tool")
 	ReactDOM.render(<App />, document.getElementById("root"));

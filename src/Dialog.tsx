d440dcddcad11ab8fd43fd4214e0227618b2c507
Sachit <sachit.shetty@gmail.com>
2019-05-12 19:05:56 -0400

    update format

--- src/Dialog.tsx
+++ src/Dialog.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -21,7 +21,7 @@ interface DialogComponentProps {
 	handleClose: Function;
 }
 
-interface DialogComponentState {}
+interface DialogComponentState { }
 
 export default class DialogComponent extends Component<DialogComponentProps, DialogComponentState> {
 	toReadableDateFormat(utcMilliseconds: number) {
@@ -37,8 +37,8 @@ export default class DialogComponent extends Component<DialogComponentProps, Dia
 	}
 
 	render() {
-		let {open, handleClose, dialogClose, handleConfirmation} = this.props;
-		let {meetupEventData, onCheck} = this.props;
+		let { open, handleClose, dialogClose, handleConfirmation } = this.props;
+		let { meetupEventData, onCheck } = this.props;
 		let greeting =
 			meetupEventData.length > 0
 				? `Here's what I found for ${meetupEventData[0]["group"]["name"]}!`
@@ -53,29 +53,29 @@ export default class DialogComponent extends Component<DialogComponentProps, Dia
 				onClose={() => handleClose()}
 				aria-labelledby='alert-dialog-slide-title'
 				aria-describedby='alert-dialog-slide-description'>
-				<DialogTitle style={{padding: "20px 24px 6px"}} id='alert-dialog-slide-title'>
+				<DialogTitle style={{ padding: "20px 24px 6px" }} id='alert-dialog-slide-title'>
 					<h1 className='habibi Dialog-header'>
 						{greeting}
 						<br />
 						{followUp}
 					</h1>
 				</DialogTitle>
-				<DialogContent style={{padding: "0px 0px 0px 22px"}}>
+				<DialogContent style={{ padding: "0px 0px 0px 22px" }}>
 					<DialogContentText id='alert-dialog-slide-description'>
 						{meetupEventData.map((x, i) => {
 							return (
 								<div>
-									<ul key={i} style={{padding: "0px 0px 18px 0px", margin: "0px"}}>
+									<ul key={i} style={{ padding: "0px 0px 18px 0px", margin: "0px" }}>
 										<div
 											style={{
 												display: "flex",
 												justifyContent: "space-between"
 											}}>
-											<li style={{color: "rgba(0, 0, 0, 0.85)"}}>
+											<li style={{ color: "rgba(0, 0, 0, 0.85)" }}>
 												{typeof x["venue"] !== "undefined" ? x["venue"]["name"] : x["group"]["name"]}
 											</li>
 											<input
-												style={{marginRight: "15px"}}
+												style={{ marginRight: "15px" }}
 												type='checkbox'
 												checked={x["checked"]}
 												onChange={(e) => onCheck(e)}
@@ -91,15 +91,15 @@ export default class DialogComponent extends Component<DialogComponentProps, Dia
 												{x["name"]}
 											</a>
 										</li>
-										<li style={{color: "rgba(0, 0, 0, 0.85)"}}>{this.toReadableDateFormat(x["time"])}</li>
+										<li style={{ color: "rgba(0, 0, 0, 0.85)" }}>{this.toReadableDateFormat(x["time"])}</li>
 									</ul>
 								</div>
 							);
 						})}
 					</DialogContentText>
 				</DialogContent>
-				<div style={{padding: "0px 20px 0px 20px"}}>
-					<h3 style={{fontSize: "13px", textShadow: "#ff8a00bd 1px 0 7px"}}>
+				<div style={{ padding: "0px 20px 0px 20px" }}>
+					<h3 style={{ fontSize: "13px", textShadow: "#ff8a00bd 1px 0 7px" }}>
 						{
 							"Warning: You must be signed into chrome or allow syncing on request for the authentication flow to work properly"
 						}
